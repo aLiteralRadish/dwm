f7841af61d2e3faa3c8f932b045d1d0673f78418
Caitlyn Harte <radish@woods.ooo>
2024-04-17 15:38:03 -0500

    mpv tag rule

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
 	{ "kitty",	  NULL,		  NULL,		  1 << 3,		0,			 -1 },
 	{ "discord",  NULL,		  NULL,		  1 << 5,		0,			 -1 },
+	{ "mpv",	  NULL,		  NULL,		  1 << 1,		0,			 -1 },
 };
 
 /* layout(s) */
@@ -65,13 +66,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_pink, "-nf", col_gray3, "-sb", col_white, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", "/home/radish", NULL };
+static const char *dmenucmd[] = 	{ "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_pink, "-nf", col_gray3, "-sb", col_white, "-sf", col_gray4, NULL };
+static const char *termcmd[]  = 	{ "kitty", "/home/radish", NULL };
+static const char *customcmd[] = 	{ "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,						XK_v,	   spawn,		   {.v = customcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
