9dace2565a2628b006ee8387f78375abc0a08982
Caitlyn Harte <radish@woods.ooo>
2024-04-17 15:07:59 -0500

    added new tag label and enforced tag rules for firefox and kitty

--- config.def.h
+++ config.def.h
@@ -24,7 +24,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰈹", "", "󰎄", "", "󱑢", " ", " ", " ", " " };
+static const char *tags[] = { "󰈹", "", "󰎄", "", "󱑢", "󰙯", " ", " ", " " };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -33,7 +33,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "kitty",	  NULL,		  NULL,		  1 << 3,		0,			 -1 },
 };
 
 /* layout(s) */
