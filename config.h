7012ad26290af857d51c73c5560c90eff7b471ee
Caitlyn Harte <radish@woods.ooo>
2024-04-17 19:57:57 -0500

    change bar back to top

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh    = 20;       /* horiz outer gap between windo
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
 static const char d_fg[]      	= "#6e6a86";
